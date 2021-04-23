68982234c02ad7235b03c82c832185ab0cf71996
sadowski.dawid <222560@edu.p.lodz.pl>
2021-04-23 17:35:35 +0200

    Add test for 10 launched missiles

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,24 +1,57 @@
 package edu.iis.mto.multithread;
 
-import static org.mockito.Mockito.verify;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.concurrent.ExecutorService;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 class RadarTest {
 
     @Mock
     private PatriotBattery batteryMock;
+    @Mock
+    private ExecutorService executorServiceMock;
+
+    @BeforeEach
+    void setUp() {
+        when(executorServiceMock.submit(any(Runnable.class))).thenAnswer(invocation -> {
+            Runnable runnable = invocation.getArgument(0);
+            runnable.run();
+            return null;
+        });
+    }
 
     @Test
-    void launchPatriotOnceWhenNoticesAScudMissle() {
-        Radar radar = new Radar(batteryMock);
-        Scud enemyMissle = new Scud();
-        radar.notice(enemyMissle);
-        verify(batteryMock).launchPatriot(enemyMissle);
+    void launchPatriotOnceWhenNoticesAScudMissile() {
+        int count = 1;
+
+        BetterRadar radar = new BetterRadar(batteryMock, count, executorServiceMock);
+        Scud enemyMissile = new Scud();
+
+        radar.notice(enemyMissile);
+
+        verify(batteryMock, times(count)).launchPatriot(enemyMissile);
+    }
+
+    @Test
+    void launchPatriotTenTimesWhenNoticesAScudMissile() {
+        int count = 10;
+
+        BetterRadar radar = new BetterRadar(batteryMock, count, executorServiceMock);
+        Scud enemyMissile = new Scud();
+
+        radar.notice(enemyMissile);
+
+        verify(batteryMock, times(count)).launchPatriot(enemyMissile);
     }
 
 }
